1b6a626e387a1a9582a82090d85105516ac044a4
Markus Kettunen <markus.kettunen@aalto.fi>
2016-11-30 16:45:07 +0200

    Initial code for HMC sampling visualization.

--- rbm.py
+++ rbm.py
@@ -430,28 +430,6 @@ def test_rbm(learning_rate=0.01, training_epochs=15,
         # serialize rbm
         pickle.dump(rbm, open(RBM_FILE, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
 
-    # TODO: Implement this sampler override!
-    #rbm.sampler = HamiltonianMonteCarloSampler(rbm.theano_rng, rbm, ...
-    #self.sampler = GibbsSampler(self.theano_rng, self)
-    #        random,
-    #        rbm,
-    #        shared_positions,
-    #        energy_fn,
-    #        initial_stepsize=0.01,
-    #        target_acceptance_rate=.9,
-    #        n_steps=20,
-    #        stepsize_dec=0.98,
-    #        stepsize_min=0.001,
-    #        stepsize_max=0.25,
-    #        stepsize_inc=1.02,
-    #        avg_acceptance_slowness=1.0,
-    #        ):
-
-    hmc_sampler = HamiltonianMonteCarloSampler(
-            theano_rng,
-            rbm,
-            rng.uniform(low=0.0, high=1.0, size=(1, 784)).astype(theano.config.floatX)
-            )
 
     #################################
     #     Sampling from the RBM     #
@@ -459,73 +437,104 @@ def test_rbm(learning_rate=0.01, training_epochs=15,
     # find out the number of test samples
     number_of_test_samples = test_set_x.get_value(borrow=True).shape[0]
 
-    # start chains from random samples
-    persistent_vis_chain = theano.shared(
-        numpy.asarray(
-            rng.uniform(
-                low=0.0,
-                high=1.0,
-                size=(n_chains, 28 * 28)
-                ),
-            dtype=theano.config.floatX
+    USE_HMC = True
+    
+    if USE_HMC:
+        # HMC sampling
+        hmc_sampler = HamiltonianMonteCarloSampler(
+            theano_rng,
+            rbm,
+            rng.uniform(low=0.0, high=1.0, size=(n_chains, 784)).astype(theano.config.floatX)
         )
-    )
-
-    # end-snippet-6 start-snippet-7
-    plot_every = 1000
-    # define one step of Gibbs sampling (mf = mean-field), define a
-    # function that does `plot_every` steps before returning the
-    # sample for plotting
-    (
-        [
-            presig_hids,
-            hid_mfs,
-            hid_samples,
-            presig_vis,
-            vis_mfs,
-            vis_samples
-        ],
-        updates
-    ) = theano.scan(
-        rbm.sampler.update_visible_hidden_visible,
-        outputs_info=[None, None, None, None, None, persistent_vis_chain],
-        n_steps=plot_every,
-        name="update_visible_hidden_visible"
-    )
-
-    # add to updates the shared variable that takes care of our persistent
-    # chain :.
-    updates.update({persistent_vis_chain: vis_samples[-1]})
-    # construct the function that implements our persistent chain.
-    # we generate the "mean field" activations for plotting and the actual
-    # samples for reinitializing the state of our persistent chain
-    sample_fn = theano.function(
-        [],
-        [
-            vis_mfs[-1],
-            vis_samples[-1]
-        ],
-        updates=updates,
-        name='sample_fn'
-    )
-
-    # create a space to store the image for plotting ( we need to leave
-    # room for the tile_spacing as well)
-    image_data = numpy.zeros(
-        (29 * n_samples + 1, 29 * n_chains - 1),
-        dtype='uint8'
-    )
-    for idx in range(n_samples):
-        # generate `plot_every` intermediate samples that we discard,
-        # because successive samples in the chain are too correlated
-        vis_mf, vis_sample = sample_fn()
-        print(' ... plotting sample %d' % idx)
-        image_data[29 * idx:29 * idx + 28, :] = tile_raster_images(
-            X=vis_mf,
-            img_shape=(28, 28),
-            tile_shape=(1, n_chains),
-            tile_spacing=(1, 1)
+        
+        # create a space to store the image for plotting ( we need to leave
+        # room for the tile_spacing as well)
+        image_data = numpy.zeros(
+            (29 * n_samples + 1, 29 * n_chains - 1),
+            dtype='uint8'
         )
+        for idx in range(n_samples):
+            # generate `plot_every` intermediate samples that we discard,
+            # because successive samples in the chain are too correlated
+            vis_mf = hmc_sampler.draw()
+            print(' ... plotting sample %d' % idx)
+            image_data[29 * idx:29 * idx + 28, :] = tile_raster_images(
+                X=vis_mf,
+                img_shape=(28, 28),
+                tile_shape=(1, n_chains),
+                tile_spacing=(1, 1)
+            )
+        
+    else:
+        # Gibbs sampling
+        
+        # start chains from random samples
+        persistent_vis_chain = theano.shared(
+            numpy.asarray(
+                rng.uniform(
+                    low=0.0,
+                    high=1.0,
+                    size=(n_chains, 28 * 28)
+                    ),
+                dtype=theano.config.floatX
+            )
+        )
+
+        # end-snippet-6 start-snippet-7
+        plot_every = 1000
+        # define one step of Gibbs sampling (mf = mean-field), define a
+        # function that does `plot_every` steps before returning the
+        # sample for plotting
+        (
+            [
+                presig_hids,
+                hid_mfs,
+                hid_samples,
+                presig_vis,
+                vis_mfs,
+                vis_samples
+            ],
+            updates
+        ) = theano.scan(
+            rbm.sampler.update_visible_hidden_visible,
+            outputs_info=[None, None, None, None, None, persistent_vis_chain],
+            n_steps=plot_every,
+            name="update_visible_hidden_visible"
+        )
+
+        # add to updates the shared variable that takes care of our persistent
+        # chain :.
+        updates.update({persistent_vis_chain: vis_samples[-1]})
+        # construct the function that implements our persistent chain.
+        # we generate the "mean field" activations for plotting and the actual
+        # samples for reinitializing the state of our persistent chain
+        sample_fn = theano.function(
+            [],
+            [
+                vis_mfs[-1],
+                vis_samples[-1]
+            ],
+            updates=updates,
+            name='sample_fn'
+        )
+
+        # create a space to store the image for plotting ( we need to leave
+        # room for the tile_spacing as well)
+        image_data = numpy.zeros(
+            (29 * n_samples + 1, 29 * n_chains - 1),
+            dtype='uint8'
+        )
+        for idx in range(n_samples):
+            # generate `plot_every` intermediate samples that we discard,
+            # because successive samples in the chain are too correlated
+            vis_mf, vis_sample = sample_fn()
+            print(' ... plotting sample %d' % idx)
+            image_data[29 * idx:29 * idx + 28, :] = tile_raster_images(
+                X=vis_mf,
+                img_shape=(28, 28),
+                tile_shape=(1, n_chains),
+                tile_spacing=(1, 1)
+            )
 
     # construct image
     image = Image.fromarray(image_data)
